d8c2f937592c4e56a5ec03195abdf35964af1850
PrabhatAcharya <code2prabhat@gmail.com>
2023-01-05 09:29:55 +0530

    NavBar_with_ChakraUi

--- discovery_plus/src/App.js
+++ discovery_plus/src/App.js
@@ -1,12 +1,13 @@
-import Card from "./Components/Kids_componanrts/Card";
-import Slider from "./Components/Kids_componanrts/kids_craousel";
+import { Navbar } from "./Components/LandingPage/navbar";
 function App() {
   return (
+  
     <div className="App">
-      <Slider/>
-      <Card/>
-     
+      <Navbar />
+      {/* <Slider/>
+      <Card/> */}
     </div>
+  
   );
 }
 
