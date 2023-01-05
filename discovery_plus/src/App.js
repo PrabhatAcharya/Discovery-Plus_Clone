37c77aaef0b83b3a8b717d0102129a08b5d99fbb
comefly304 <114000460+comefly304@users.noreply.github.com>
2023-01-05 10:03:54 +0530

    explore , nav bar and footer is remsaining

--- discovery_plus/src/App.js
+++ discovery_plus/src/App.js
@@ -1,10 +1,10 @@
 import Card from "./Components/Kids_componanrts/Card";
 import Slider from "./Components/Kids_componanrts/kids_craousel";
+import { Explore } from "./Components/explore/explore";
 function App() {
   return (
     <div className="App">
-      <Slider/>
-      <Card/>
+      <Explore />
      
     </div>
   );
---
+++ discovery_plus/src/Components/AllRouter/router.jsx
@@ -0,0 +1,8 @@
+const explore=()=>{
+    return <>
+    <div id="screen" style={{height: "100%", width: "100%", backgroundColor: "#1a1c21"}}>
+
+    </div>
+    
+    </>
+}
