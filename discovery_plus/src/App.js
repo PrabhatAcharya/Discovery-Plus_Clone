925117c16ecd491429a919222f779077ae1f03ef
PrabhatAcharya <code2prabhat@gmail.com>
2023-01-05 17:00:52 +0530

    mainUpdate

---
+++ discovery_plus/src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from "./Components/LandingPage/navbar";
+function App() {
+  return (
+  
+    <div className="App">
+      <Navbar />
+
+    </div>
+  
+  );
+}
+
+export default App;
