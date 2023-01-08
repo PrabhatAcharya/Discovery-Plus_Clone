a3f6e49e08b0b9100db408f6b0ab481d73a0f826
comefly304 <114000460+comefly304@users.noreply.github.com>
2023-01-08 20:46:20 +0530

    explrore

--- discovery_plus/src/App.js
+++ discovery_plus/src/App.js
@@ -1,6 +1,6 @@
 
 import { Explore } from "./Components/explore/explore.jsx";
-import { Footer } from "./Components/explore/footer/footer.jsx";
+
 import ALLRouter from "./Components/AllRouter/router";
 import { Navbar } from "./Components/LandingPage/navbar";
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <ALLRouter />
-      <Footer />
+      
       
 
     </div>
